b09e808d2707aa63a911e308cec151e749354376
helloworld-lyh <helloworld-lyh@outlook.com>
2024-03-17 22:40:03 +0800

    世界征服者4意淫将领生成器

---
+++ assets/js/general.js
@@ -0,0 +1,9 @@
+var money = $('#money_val');
+var medal = $('#medal_val');
+var blue = $('#blue_val');
+var purple = $('#purple_val');
+var tech = $('#tech_val');
+var yellow = $('#yellow_val');
+function change_res(){
+    
+}
